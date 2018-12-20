fa17853db6948d4bc9bfd82a05dbc502a57e007f
Petter Rasmussen <petter.rasmussen@gmail.com>
2018-12-20 17:44:03 +0100

    Simple design + add stylish-haskell config

--- src/Week/WeekInfo.hs
+++ src/Week/WeekInfo.hs
@@ -4,15 +4,15 @@ module Week.WeekInfo
     ) where
 
 
-import qualified Data.Time.Clock as Clock
+import Data.Aeson ((.=))
+import qualified Data.Aeson as Aeson
+import qualified Data.ByteString.Lazy.Char8 as BSL8
+import Data.Monoid ((<>))
 import qualified Data.Time.Calendar as Calendar
 import qualified Data.Time.Calendar.WeekDate as WeekDate
-import qualified Data.ByteString.Lazy.Char8 as BSL8
-import qualified Data.Aeson as Aeson
-import qualified Servant
-import Data.Monoid ((<>))
+import qualified Data.Time.Clock as Clock
 import Lucid
-import Data.Aeson ((.=))
+import qualified Servant
 
 
 
@@ -33,11 +33,7 @@ instance ToHtml WeekInfo where
                 title_ ("Week: " <> week)
                 link_ [ rel_ "stylesheet", type_ "text/css", href_ "/static/styles.css" ]
             body_ $ do
-                div_ [ class_ "week" ] ("Week: " <> week)
-                div_ [ class_ "api" ] $ do
-                    h3_ "API"
-                    p_ "Plaintext: curl https://week.vevapp.no"
-                    p_ "JSON: curl https://week.vevapp.no --header \"Accept: application/json\""
+                h1_ week
 
     toHtmlRaw =
         toHtml
